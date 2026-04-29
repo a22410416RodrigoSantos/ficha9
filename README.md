4e99cc5bea758fc82abd1d86bc279d886fbd0119
a22410416RodrigoSantos <a22410416@alunos.ulht.pt>
2026-04-29 08:35:33 +0000

    ficha 9

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # festival
 
 Descreva aqui as alterações/correções que fez
+
+## 1
+- adicionei ".order_by("data")" em views.py para ordenar pela data.
+
+## 2 
+- a linha "ordering = ["dia__data", "hora"]" ordena os palcos pelo dia e depois pela hora.
+
+## 3
+- O formulário ConcertoForm foi alterado para incluir todos os campos do modelo (banda, dia, hora, palco), permitindo a edição completa de um concerto.
+- O template editar_concerto.html foi corrigido para apresentar informação do concerto
+
+## 4
+- Foi criada a rota concertos/<int:concerto_id>/apagar/
+- Foi implementada a view apagar_concerto_view
+- Foi adicionado o botão "Apagar concerto" no template concerto.html
+
+## 5
+- Criei a rota concertos/criar/
+- Implementei a view criar_concerto_view
+- Criei template criar_concerto.html
+- Coloquei o link certo para o "criar_concerto.html" no layout.html
--- festival/views.py
+++ festival/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Dia, Palco, Concerto               
 from .forms import ConcertoForm, PalcoForm
+from django.db.models import Count
 
 
 def index_view(request):
     return render(request, 'festival/index.html')
 
 def dias_view(request):
-    dias = Dia.objects.all()
+    dias = Dia.objects.all().order_by("data")
 
     context = {'dias': dias}
 
@@ -15,7 +16,7 @@ def dias_view(request):
 
 
 def palcos_view(request):
-    palcos = Palco.objects.all() 
+    palcos = Palco.objects.annotate(num_concertos=Count("concertos"))
 
     context = {'palcos': palcos}
 
@@ -47,3 +48,45 @@ def editar_concerto_view(request, concerto_id):
     }
 
     return render(request, 'festival/editar_concerto.html', context)
+
+def apagar_concerto_view(request, concerto_id):
+    concerto = get_object_or_404(Concerto, id=concerto_id)
+
+    if request.method == 'POST':
+        concerto.delete()
+        return redirect('dias')  
+
+    return redirect('concerto', concerto_id=concerto.id)
+
+def criar_concerto_view(request):
+    if request.method == 'POST':
+        form = ConcertoForm(request.POST)
+        if form.is_valid():
+            concerto = form.save()
+            return redirect('concerto', concerto_id=concerto.id)
+    else:
+        form = ConcertoForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'festival/criar_concerto.html', context)
+
+def editar_palco_view(request, palco_id):
+    palco = get_object_or_404(Palco, id=palco_id)
+
+    if request.method == 'POST':
+        form = PalcoForm(request.POST, request.FILES, instance=palco)
+        if form.is_valid():
+            form.save()
+            return redirect('palcos')
+    else:
+        form = PalcoForm(instance=palco)
+
+    context = {
+        'form': form,
+        'palco': palco,
+    }
+
+    return render(request, 'festival/editar_palco.html', context)
